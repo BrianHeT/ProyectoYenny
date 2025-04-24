b3d30641ae4b00f8bf33c8e3f67834701d8381fd
HectorAG05 <hectoralex05@gmail.com>
2025-04-24 09:25:40 -0300

    prueba de guardado
    prueba

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 JOptionPane.showMessageDialog(null, "hola negros");
+
+	JOptionPane.showMessageDialog(null, "negro tu, negrisimo");
 	}
 
 }
