8f5e57ab5e6c92c237c1c3013b3ded24ae75ff2b
HectorAG05 <hectoralex05@gmail.com>
2025-06-12 12:18:34 -0300

    hola

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,10 +1,14 @@
 package GUI;
 
+import javax.swing.JOptionPane;
+
 import DLL.ControllerUsuario;
 
 public class Main {
     public static void main(String[] args) {
         ControllerUsuario controller = new ControllerUsuario();
         new MainFrame(controller); // Show login window
+        
+        JOptionPane.showMessageDialog(null, "Soy hector");
     }
 }
